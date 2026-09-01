47f08721a163d1e48e3f099d19dc0d687400138c
Luvly Sharma <er.luvly.sharma@gmail.com>
2026-09-01 19:02:16 +0530

    t

---
+++ Rotate_Array.py
@@ -0,0 +1,18 @@
+class Solution:
+    def rotateArr(self, arr, d):
+        # code here
+        n = len(arr)
+        d = d % n
+        
+        
+        self.reverse(arr, 0, d-1)
+        self.reverse(arr, d, n-1)
+        self.reverse(arr, 0, n-1)
+        
+        
+    def reverse(self,arr, left, right):
+        while left < right:
+            arr[left], arr[right] = arr[right], arr[left]
+            left += 1
+            right -=1
+        
